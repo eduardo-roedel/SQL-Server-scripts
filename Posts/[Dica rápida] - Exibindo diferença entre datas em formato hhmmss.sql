67f9e8b59bb198f32fcab100953c8d45e38cea13
Eduardo Roedel <eduardo_roedel@hotmail.com>
2021-06-15 22:32:10 -0300

    .

--- Posts/[Dica rápida] - Exibindo diferença entre datas em formato hhmmss.sql
+++ Posts/[Dica rápida] - Exibindo diferença entre datas em formato hhmmss.sql
@@ -2,7 +2,6 @@
 
 DECLARE @data1 DATETIME = '2021-06-12 19:00:01.000';
 DECLARE @data2 DATETIME = '2021-06-12 20:00:00.000';
---03h 59m 59s
 
 SELECT
 	CONCAT( RIGHT(CONCAT('0', ((QueryResult.ColumnName / 60) / 60)) , 2) , ':', RIGHT(CONCAT('0', (QueryResult.ColumnName % 60)), 2) , ':',	RIGHT(CONCAT('0', ((QueryResult.ColumnName / 60) % 60)), 2) ) AS HoraVarchar	
